b58d5433a53dd4c4207f94cd6e8dba4059102078
weishaoji <weishaoji@myquant.cn>
2016-05-11 16:48:10 +0800

    Fix EMA, use NaN for empty value

--- ta-lib.ema/index.js
+++ ta-lib.ema/index.js
@@ -3,17 +3,13 @@ export default function ema(close, timeperiod = 30) {
   var alpha = 2 / (timeperiod + 1)
   var previous
   var initialAccumulator = 0
-  var skip = 0
 
   return close.map((v, i) => {
     if (!Number.isFinite(v)) throw new Error('Input value should be a number!')
-    if (!previous) {
-      skip++
-      return undefined
-    } else if (i < timeperiod + skip - 1) {
+    if (i < timeperiod - 1) {
       initialAccumulator += v
-      return undefined
-    } else if (i === timeperiod + skip - 1) {
+      return NaN
+    } else if (i === timeperiod - 1) {
       initialAccumulator += v
       var initialValue = initialAccumulator / timeperiod
       previous = initialValue
--- ta-lib.sma/index.js
+++ ta-lib.sma/index.js
@@ -6,7 +6,7 @@ export default function sma(values, timeperiod = 30) {
     if (!Number.isFinite(v)) throw new Error('Input value should be a number!')
     window.push(v)
     if (i < timeperiod - 1) {
-      return null
+      return NaN
     } else {
       if (i >= timeperiod) window.splice(0, 1)
       return window.reduce((a, b) => a + b) / timeperiod
