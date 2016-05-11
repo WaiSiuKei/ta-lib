5580391a18f86f27ae0332b4053073143603bf57
weishaoji <weishaoji@myquant.cn>
2016-05-11 16:31:58 +0800

    Add SMA

---
+++ ta-lib.sma/README.md
@@ -0,0 +1,12 @@
+# ta-lib.ema
+
+The ta-lib method `sma` exported as an ES6 module.
+
+## Installation
+
+TODO
+
+In ES6:
+```js
+import ema from 'talib.sma'
+```
---
+++ ta-lib.sma/index.js
@@ -0,0 +1,15 @@
+export default function sma(values, timeperiod = 30) {
+  if (!Number.isFinite(timeperiod)) throw new Error('Timeperiod should be a number!')
+  var window = []
+
+  return values.map((v, i) => {
+    if (!Number.isFinite(v)) throw new Error('Input value should be a number!')
+    window.push(v)
+    if (i < timeperiod - 1) {
+      return null
+    } else {
+      if (i >= timeperiod) window.splice(0, 1)
+      return window.reduce((a, b) => a + b) / timeperiod
+    }
+  })
+}
