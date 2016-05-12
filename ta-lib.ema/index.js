7edece67336c7bb247c6e8d2f0e34ea0c3865ee9
weishaoji <weishaoji@myquant.cn>
2016-05-12 16:23:36 +0800

    Update EMA

--- ta-lib.ema/index.js
+++ ta-lib.ema/index.js
@@ -3,19 +3,26 @@ export default function ema(close, timeperiod = 30) {
   var alpha = 2 / (timeperiod + 1)
   var previous
   var initialAccumulator = 0
+  var skip = 0
 
   return close.map((v, i) => {
-    if (!Number.isFinite(v)) throw new Error('Input value should be a number!')
-    if (i < timeperiod - 1) {
+    if (!Number.isFinite(v)) {
+      if (isNaN(v)) {
+        skip += 1
+        return NaN
+      } else {
+        throw new Error('Input value should be a number!')
+      }
+    } else if (i < timeperiod + skip - 1) {
       initialAccumulator += v
       return NaN
-    } else if (i === timeperiod - 1) {
+    } else if (i === timeperiod + skip - 1) {
       initialAccumulator += v
-      var initialValue = initialAccumulator / timeperiod
+      let initialValue = initialAccumulator / timeperiod
       previous = initialValue
       return initialValue
     } else {
-      var nextValue = v * alpha + (1 - alpha) * previous
+      let nextValue = alpha * (v - previous) + previous
       previous = nextValue
       return nextValue
     }
