dcfc1e5b9fa554da66a59fcdc4b8831aa8ed924a
weishaoji <weishaoji@myquant.cn>
2016-05-12 16:24:01 +0800

    Updata SMA

--- ta-lib.sma/index.js
+++ ta-lib.sma/index.js
@@ -1,14 +1,25 @@
 export default function sma(values, timeperiod = 30) {
   if (!Number.isFinite(timeperiod)) throw new Error('Timeperiod should be a number!')
   var window = []
+  var skip = 0
 
   return values.map((v, i) => {
-    if (!Number.isFinite(v)) throw new Error('Input value should be a number!')
-    window.push(v)
-    if (i < timeperiod - 1) {
+    if (!Number.isFinite(v)) {
+      if (isNaN(v)) {
+        skip += 1
+        return NaN
+      } else {
+        throw new Error('Input value should be a number!')
+      }
+    } else if (i < timeperiod + skip - 1) {
+      window.push(v)
       return NaN
+    } else if (i == timeperiod + skip - 1){
+      window.push(v)
+      return window.reduce((a, b) => a + b) / timeperiod
     } else {
-      if (i >= timeperiod) window.splice(0, 1)
+      window.push(v)
+      window.splice(0, 1)
       return window.reduce((a, b) => a + b) / timeperiod
     }
   })
