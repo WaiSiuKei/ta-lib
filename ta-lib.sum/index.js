622e2a982085715414ca0a084823b2d97032d9be
wakiki <wakiki@outlook.com>
2016-05-15 14:13:55 +0800

    Add sum

--- ta-lib.sum/index.js
+++ ta-lib.sum/index.js
@@ -1,5 +1,8 @@
 var sum = function (values) {
+  if (!Array.isArray(values)) throw new Error('Input should be an array!')
+  if (!values.length) throw new Error('Input should not be an empty array!')
   return values.reduce(function (sum, value) {
+    if (!Number.isFinite(value) || !Number.isFinite(sum)) throw new Error('Each value should be a number!')
     return sum + value
   }, 0)
 }
