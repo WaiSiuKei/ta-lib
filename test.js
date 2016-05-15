0c9416f46ca3dba83cabcfe5674a593c2a6f4081
wakiki <wakiki@outlook.com>
2016-05-15 14:14:02 +0800

    Update test

--- test.js
+++ test.js
@@ -1,7 +1,13 @@
 import emaTest from './ta-lib.ema/test'
 import smaTest from './ta-lib.sma/test'
 import macdTest from './ta-lib.macd/test'
+import stddevTest from './ta-lib.stddev/test'
+import sumTest from './ta-lib.sum/test'
+import averageTest from './ta-lib.sum/test'
 
 describe('ta-lib.ema', emaTest)
 describe('ta-lib.sma', smaTest)
 describe('ta-lib.macd', macdTest)
+describe('ta-lib.stddev', stddevTest)
+describe('ta-lib.sum', sumTest)
+describe('ta-lib.average', averageTest)
